3790c7f35a1f21e92cdede98b48d49ef037007e7
dendeganesh <dendeganesh27@gmail.com>
2022-05-31 23:14:12 +0530

    sonar cloud

--- green-commute/src/main/java/com/zemoso/greencommute/GreenCommuteApplication.java
+++ green-commute/src/main/java/com/zemoso/greencommute/GreenCommuteApplication.java
@@ -7,8 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class GreenCommuteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreenCommuteApplication.class, args);
+	public static  void main(String[] args) {
+
+		SpringApplication.run(GreenCommuteApplication.class);
 	}
 
 }
+//./mvnw verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=ganeshdende -Dsonar.login=51c3738f43962dac8595df6385018f19e2ae2b08 -Dsonar.projectKey=ganeshdende_green_commute
+
