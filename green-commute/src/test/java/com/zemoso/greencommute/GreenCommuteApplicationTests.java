8e07b70beb1a4941f8919fdc1ca494da5c959683
dendeganesh <dendeganesh27@gmail.com>
2022-06-01 16:18:58 +0530

    added some test cases

--- green-commute/src/test/java/com/zemoso/greencommute/GreenCommuteApplicationTests.java
+++ green-commute/src/test/java/com/zemoso/greencommute/GreenCommuteApplicationTests.java
@@ -5,10 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GreenCommuteApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
+
+	@Test
+	void contextLoads() {
+	}
 
 }
-//./mvnw verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=ganeshdende -Dsonar.login=51c3738f43962dac8595df6385018f19e2ae2b08 -Dsonar.projectKey=ganeshdende_green_commute
